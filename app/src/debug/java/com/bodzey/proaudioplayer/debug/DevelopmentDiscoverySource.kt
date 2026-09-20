3b216dcbfffa8273bc51052e8afd70766d693afc
Bohdan Cherniak <70845142+bodzey@users.noreply.github.com>
2026-09-20 05:49:27 +0300

    debug: provide demo discovery source

---
+++ app/src/debug/java/com/bodzey/proaudioplayer/debug/DevelopmentDiscoverySource.kt
@@ -0,0 +1,10 @@
+package com.bodzey.proaudioplayer.debug
+
+import com.bodzey.proaudioplayer.core.discovery.DeviceDiscoverySource
+import com.bodzey.proaudioplayer.core.discovery.demo.DemoDeviceDiscoverySource
+import com.bodzey.proaudioplayer.core.discovery.demo.DemoDiscoveryController
+
+fun createDevelopmentDiscoverySource(
+    controller: DemoDiscoveryController,
+): DeviceDiscoverySource =
+    DemoDeviceDiscoverySource(controller)
