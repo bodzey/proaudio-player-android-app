81d17097cbd3c790dbf2eac438126ad5a255b3c6
Bohdan Cherniak <70845142+bodzey@users.noreply.github.com>
2026-09-20 04:57:35 +0300

    build: add coroutine and unit-test dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,6 +54,9 @@ dependencies {
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.11.0")
+
+    testImplementation("junit:junit:4.13.2")
 
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
