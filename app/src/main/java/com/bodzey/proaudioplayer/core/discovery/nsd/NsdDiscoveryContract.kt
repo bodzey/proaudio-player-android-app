2201c23134417c12e57e78534e882f06dadb6ba7
Bohdan Cherniak <70845142+bodzey@users.noreply.github.com>
2026-09-20 05:13:34 +0300

    discovery: define ProAudio DNS-SD contract

---
+++ app/src/main/java/com/bodzey/proaudioplayer/core/discovery/nsd/NsdDiscoveryContract.kt
@@ -0,0 +1,9 @@
+package com.bodzey.proaudioplayer.core.discovery.nsd
+
+object NsdDiscoveryContract {
+    const val SERVICE_TYPE = "_proaudio-player._tcp."
+
+    const val TXT_DEVICE_ID = "id"
+    const val TXT_API_MAJOR_VERSION = "api"
+    const val TXT_DISPLAY_NAME = "name"
+}
