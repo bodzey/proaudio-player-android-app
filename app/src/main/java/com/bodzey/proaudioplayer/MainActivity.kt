c928a6638c9c8983d6920f2da76f5aae1714adef
Bohdan Cherniak <70845142+bodzey@users.noreply.github.com>
2026-09-20 05:47:38 +0300

    app: wire devices view model

--- app/src/main/java/com/bodzey/proaudioplayer/MainActivity.kt
+++ app/src/main/java/com/bodzey/proaudioplayer/MainActivity.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.bodzey.proaudioplayer.ui.ProAudioPlayerApp
+import com.bodzey.proaudioplayer.ui.devices.DevicesViewModel
 import com.bodzey.proaudioplayer.ui.theme.ProAudioPlayerTheme
 
 class MainActivity : ComponentActivity() {
@@ -12,9 +14,21 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
 
+        val container = (application as ProAudioPlayerApplication).container
+
         setContent {
+            val devicesViewModel: DevicesViewModel = viewModel(
+                factory = DevicesViewModel.factory(
+                    deviceRegistry = container.deviceRegistry,
+                    demoDiscoveryController = container.demoDiscoveryController,
+                ),
+            )
+
             ProAudioPlayerTheme {
-                ProAudioPlayerApp()
+                ProAudioPlayerApp(
+                    viewModel = devicesViewModel,
+                    showDemoControls = BuildConfig.DEBUG,
+                )
             }
         }
     }
