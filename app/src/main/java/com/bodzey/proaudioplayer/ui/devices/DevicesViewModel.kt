3829cfee7a887244ee278de5df9b0fbcd3a47780
Bohdan Cherniak <70845142+bodzey@users.noreply.github.com>
2026-09-20 15:35:46 +0300

    architecture: remove session ownership from device list

--- app/src/main/java/com/bodzey/proaudioplayer/ui/devices/DevicesViewModel.kt
+++ app/src/main/java/com/bodzey/proaudioplayer/ui/devices/DevicesViewModel.kt
@@ -7,14 +7,11 @@ import androidx.lifecycle.viewmodel.viewModelFactory
 import com.bodzey.proaudioplayer.core.device.AvailableDevice
 import com.bodzey.proaudioplayer.core.device.DeviceRepository
 import com.bodzey.proaudioplayer.core.discovery.demo.DemoDiscoveryController
-import com.bodzey.proaudioplayer.core.model.DeviceId
-import com.bodzey.proaudioplayer.core.session.PlayerSessionRepository
 import kotlinx.coroutines.flow.StateFlow
 
 class DevicesViewModel(
     deviceRepository: DeviceRepository,
     private val demoDiscoveryController: DemoDiscoveryController,
-    private val sessionRepository: PlayerSessionRepository,
 ) : ViewModel() {
 
     val devices: StateFlow<List<AvailableDevice>> = deviceRepository.devices
@@ -24,22 +21,16 @@ class DevicesViewModel(
         demoDiscoveryController.setEnabled(enabled)
     }
 
-    fun selectDevice(deviceId: DeviceId) {
-        sessionRepository.select(deviceId)
-    }
-
     companion object {
         fun factory(
             deviceRepository: DeviceRepository,
             demoDiscoveryController: DemoDiscoveryController,
-            sessionRepository: PlayerSessionRepository,
         ): ViewModelProvider.Factory =
             viewModelFactory {
                 initializer {
                     DevicesViewModel(
                         deviceRepository = deviceRepository,
                         demoDiscoveryController = demoDiscoveryController,
-                        sessionRepository = sessionRepository,
                     )
                 }
             }
